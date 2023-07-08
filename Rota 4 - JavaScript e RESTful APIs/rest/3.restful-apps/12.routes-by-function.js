f7b411a64cd63cebb2c0104040077dfd664130d8
IgorAugust0 <augustoigor269@gmail.com>
2023-07-08 11:29:15 -0300

    add arrow function

--- Rota 4 - JavaScript e RESTful APIs/rest/3.restful-apps/12.routes-by-function.js
+++ Rota 4 - JavaScript e RESTful APIs/rest/3.restful-apps/12.routes-by-function.js
@@ -6,7 +6,7 @@
 // GET http://localhost:3000/lista {nome: "Material Escolar"}
 
 // req = request / res = response
-app.post("/lista", function (req, res) {
+app.post("/lista", (req, res) => {
   // Pega o nome do body do request
   var nome = req.body.nome;
   // Criar nova lista
@@ -28,7 +28,7 @@ app.post("/lista", function (req, res) {
 // RENOMEAR LISTA
 // PUT http://localhost:3000/lista/lista-0 {nome: "Supermercado"}
 
-app.put("/lista/:id_lista", function (req, res) {
+app.put("/lista/:id_lista", (req, res) => {
   // Pega o novo nome do body do request
   var novoNome = req.body.nome;
   // Pega o id da lista a ser renomeada
@@ -43,7 +43,7 @@ app.put("/lista/:id_lista", function (req, res) {
 // APAGAR LISTA
 // DELETE http://localhost:3000/lista/lista-0
 
-app.delete("/lista/:id_lista", function (req, res) {
+app.delete("/lista/:id_lista", (req, res) => {
   // Pega o id da lista a ser apagada
   var idLista = req.params.id_lista;
   // Apagar lista
@@ -56,7 +56,7 @@ app.delete("/lista/:id_lista", function (req, res) {
 // LISTAR TODAS AS LISTAS
 // GET http://localhost:3000/lista
 
-app.get("/lista", function (req, res) {
+app.get("/lista", (req, res) => {
   // Listar todas as listas
   var result = listaDAO.getListas();
   res.status(200);
@@ -69,7 +69,7 @@ app.get("/lista", function (req, res) {
 // POST http://localhost:3000/lista/lista-0 { "descricao": "Leite" }
 // POST http://localhost:3000/lista/lista-0 { "descricao": "Ovos" }
 
-app.post("/lista/:id_lista", function (req, res) {
+app.post("/lista/:id_lista", (req, res) => {
   // Pega as informações provenientes da request (body e parâmetros)
   var idLista = req.params.id_lista;
   var descricao = req.body.descricao;
@@ -83,7 +83,7 @@ app.post("/lista/:id_lista", function (req, res) {
 // ALTERAR TAREFA PARA COMPLETA OU NÃO COMPLETA
 // PUT http://localhost:3000/lista/lista-0/tarefa-0
 
-app.put("/lista/:id_lista/tarefa/:id_tarefa", function (req, res) {
+app.put("/lista/:id_lista/tarefa/:id_tarefa", (req, res) => {
   // Pega as informações provenientes da request (body e parâmetros)
   var idLista = req.params.id_lista;
   var idTarefa = req.params.id_tarefa;
@@ -97,7 +97,7 @@ app.put("/lista/:id_lista/tarefa/:id_tarefa", function (req, res) {
 // APAGAR TAREFA EM UMA LISTA
 // DELETE http://localhost:3000/lista/lista-0/tarefa/tarefa-1
 
-app.delete("/lista/:id_lista/tarefa/:id_tarefa", function (req, res) {
+app.delete("/lista/:id_lista/tarefa/:id_tarefa", (req, res) => {
   // Pega as informações provenientes da request (body e parâmetros)
   var idLista = req.params.id_lista;
   var idTarefa = req.params.id_tarefa;
@@ -111,7 +111,7 @@ app.delete("/lista/:id_lista/tarefa/:id_tarefa", function (req, res) {
 // LISTAR TAREFAS DE UMA LISTA
 // GET http://localhost:3000/lista/lista-0/tarefa
 
-app.get("/lista/:id_lista/tarefa", function (req, res) {
+app.get("/lista/:id_lista/tarefa", (req, res) => {
   // Pega o id da lista a ser listada
   var idLista = req.params.id_lista;
   // Listar tarefas da lista
